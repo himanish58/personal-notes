b9df86a73c123eaacdfb25a310d14baf149292da
himanish58 <himanish.kabiraj@gmail.com>
2020-04-01 02:24:08 +0530

    Created globalstyles.

---
+++ frontend/src/GlobalStyles.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components/macro';
+
+export default createGlobalStyle`
+	body {
+	font-family: 'Roboto' !important;
+	background: #e7eeef;
+	margin: 0;
+}
+`;
